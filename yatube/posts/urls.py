83a740ce518c680e9c59eb827a57858082360fa5
Антон Боровский <antonio9409@gmail.com>
2022-12-15 18:26:33 +0300

    Yatube project 1

---
+++ yatube/posts/urls.py
@@ -0,0 +1,13 @@
+# posts/urls.py
+from django.urls import path
+from . import views
+
+# Эта строчка обязательна. 
+# Без неё namespace работать не будет:
+# namespace должен быть объявлен при include и тут, в app_name
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list')
+] 
