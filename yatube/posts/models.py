5a4817817db24d75506f4aedefc6e586b4d4ba55
Антон Боровский <antonio9409@gmail.com>
2022-12-20 20:24:54 +0300

    Правки2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -10,7 +10,7 @@ class Group(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
-    def __str__(self):  
+    def __str__(self):
         return self.title
 
 
@@ -32,6 +32,6 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-pub_date']
-    
+
     def __str__(self):
         return self.text
